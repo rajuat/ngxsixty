62311db181dd7bbccb9fa3f62626fdbe3e615de3
raju.athokpam <arc783bye875#>
2017-05-31 14:53:18 +0530

    bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MdIconModule } from '@angular/material';
 import { MdSidenavModule } from '@angular/material';
 
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+
 import { AppComponent } from './app.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { WhyUsComponent } from './why-us/why-us.component';
@@ -18,11 +20,12 @@ import { DietComponent } from './diet/diet.component';
 import { ScheduleComponent } from './schedule/schedule.component';
 import { ContactComponent } from './contact/contact.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 
 
 const materialModules = [
   BrowserAnimationsModule,
-  NoopAnimationsModule,  
+  NoopAnimationsModule,
   MdIconModule,
   MdSidenavModule
 ];
@@ -35,13 +38,15 @@ const materialModules = [
     DietComponent,
     ScheduleComponent,
     ContactComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
     FormsModule,
-    HttpModule,    
+    HttpModule,
+    CarouselModule.forRoot(),
   ].concat(materialModules),
 
   providers: [],
--- src/app/router/app-routing.module.ts
+++ src/app/router/app-routing.module.ts
@@ -3,16 +3,18 @@ import { ScheduleComponent } from './../schedule/schedule.component';
 import { DietComponent } from './../diet/diet.component';
 import { WhyUsComponent } from './../why-us/why-us.component';
 import { AboutUsComponent } from './../about-us/about-us.component';
+import { HomeComponent } from "./../home/home.component";
 import { Routes, RouterModule } from "@angular/router"
 import { NgModule } from "@angular/core";
 
 const routes: Routes = [
-    { path: '', redirectTo: '/', pathMatch: 'full' },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutUsComponent },
     { path: 'why', component: WhyUsComponent },
     { path: 'diet', component: DietComponent },
     { path: 'schedule', component: ScheduleComponent },
-    { path: 'contact', component: ContactComponent }    
+    { path: 'contact', component: ContactComponent }
 ];
 
 @NgModule({
